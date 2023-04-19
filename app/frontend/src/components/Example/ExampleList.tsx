2c434c82594a4a66ed032a60f88886f0ba9526d2
Igor Dos Santos - MSFT <igorsan@outlook.com>
2023-04-19 12:22:44 -0400

    Customizations - Adding OriBot and Nestlé references
    Customizations - Adding OriBot and Nestlé references

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -9,11 +9,11 @@ export type ExampleModel = {
 
 const EXAMPLES: ExampleModel[] = [
     {
-        text: "What is included in my Northwind Health Plus plan that is not in standard?",
-        value: "What is included in my Northwind Health Plus plan that is not in standard?"
+        text: "What were Nescafé's sales in 2021?",
+        value: "What were Nescafé's sales in 2021?"
     },
-    { text: "What happens in a performance review?", value: "What happens in a performance review?" },
-    { text: "What does a Product Manager do?", value: "What does a Product Manager do?" }
+    { text: "In which countries is Nescau sold?", value: "In which countries is Nescau sold?" },
+    { text: "What's Nestlé's most popular product in Chile?", value: "What's Nestlé's most popular product in Chile?" }
 ];
 
 interface Props {
--- app/frontend/src/pages/chat/Chat.tsx
+++ app/frontend/src/pages/chat/Chat.tsx
@@ -181,7 +181,7 @@ const Chat = () => {
                     <div className={styles.chatInput}>
                         <QuestionInput
                             clearOnSend
-                            placeholder="Type a new question (e.g. does my plan cover annual eye exams?)"
+                            placeholder="Type a new question (e.g. What was Nescafé's sales in 2021?)"
                             disabled={isLoading}
                             onSend={question => makeApiRequest(question)}
                         />
